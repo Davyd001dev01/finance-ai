be1cd841ef93d933552e0710752d938bf92d7f39
Davyd001dev01 <davyd7dev01@gmail.com>
2024-11-17 12:41:32 -0300

    feat: ensure transaction can be updated

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -7,10 +7,11 @@ import {
   TransactionPaymentMethod,
   TransactionType,
 } from "@prisma/client";
-import { addTransactionSchema } from "./schema";
+import { upsertTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
-interface AddTransactionParams {
+interface UpsertTransactionParams {
+  id?: string;
   name: string;
   amount: number;
   type: TransactionType;
@@ -20,16 +21,24 @@ interface AddTransactionParams {
 }
 
 //Omit<Prisma.TransactionCreateInput, 'userId'> outra forma de typa
-export const addTransaction = async (params: AddTransactionParams) => {
+export const upsertTransaction = async (params: UpsertTransactionParams) => {
   //autenticação da server action
-  addTransactionSchema.parse(params);
+  upsertTransactionSchema.parse(params);
 
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  await db.transaction.create({
-    data: { ...params, userId },
+  // await db.transaction.create({
+  //   data: { ...params, userId },
+  // });
+
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
+    },
+    update: { ...params, userId },
+    create: { ...params, userId },
   });
   revalidatePath("/transactions");
 };
--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -4,12 +4,13 @@ import { Transaction, TransactionType } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 // import TransactionTypeBadge from "../_components/type-badge";
 import { Badge } from "@/app/_components/ui/badge";
-import { Circle, PencilIcon, Trash } from "lucide-react";
+import { Circle, Trash } from "lucide-react";
 import { Button } from "@/app/_components/ui/button";
 import {
   TRANSACTION_CATEGORY_LABELS,
   TRANSACTION_PAYMENT_METHOD_LABELS,
 } from "@/app/_constants/transactions";
+import EditTransactionButton from "../_components/edit-transaction-button";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -78,12 +79,10 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "action",
     header: "",
-    cell: () => {
+    cell: ({ row: { original: transaction } }) => {
       return (
         <div className="space-x-1">
-          <Button variant="ghost" size="icon" className="text-muted-foreground">
-            <PencilIcon />
-          </Button>
+          <EditTransactionButton transaction={transaction} />
           <Button variant="ghost" size="icon" className="text-muted-foreground">
             <Trash />
           </Button>
